846a2bc3b53b77b332defe52f2ef595f1d400b85
Atom Yah <atom@yah.bz>
2024-03-23 05:14:00 +0000

    kako wa dame

--- index.js
+++ index.js
@@ -550,6 +550,10 @@ const orderChoice = (ev) => {
 
 // LINE Flex Message（予約希望日を聞く）を表示するaskDate関数. askDate(ev,orderedMenu)のorderedMenuには1,2,3のいずれかの文字列が入ってる．
 const askDate = (ev,orderedMenu) => {
+  const now = new Date();
+  const minDateTime = now.toISOString().slice(0, 16); // 現在日時を"YYYY-MM-DDThh:mm"の形式で取得
+  /// 585行目、"min":`${minDateTime}`で過去の日時には予約できなくする．
+
   return client.replyMessage(ev.replyToken,{
       "type":"flex",
       "altText":"予約日選択",
@@ -577,7 +581,8 @@ const askDate = (ev,orderedMenu) => {
                 "type": "datetimepicker",
                 "label": "希望日を選択する",
                 "data": `date&${orderedMenu}`,
-                "mode": "date"
+                "mode": "date",
+                "min": `${minDateTime}`
               }
             }
           ]
